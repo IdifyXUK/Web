a918240fa0f876cc6d7a2e6b3b8dd89921143277
kevinkandiaz <kevin.kan.diaz@gmail.com>
2017-04-22 20:44:40 +0800

    Add Sign Up and Login router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Idify' });
 });
 
+router.get('/joinidify', function(req, res, next){
+	res.render('signup', {title: 'Join Idify'})
+});
+
+router.get('/login', function(req, res, next){
+	res.render('login', {title: 'Idify'})
+});
+
 module.exports = router;
